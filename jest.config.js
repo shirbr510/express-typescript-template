0cc6cde0e11dc3386bc2d468dd2533f8603133c5
Shir Brass <My Email>
2024-03-10 15:11:07 +0200

    add sample test

--- jest.config.js
+++ jest.config.js
@@ -3,14 +3,6 @@ const config = {
   silent: true,
   clearMocks: true,
   testEnvironment: 'node',
-  reporters: [
-    'default',
-    ['jest-junit', { outputDirectory: './test-reports/junit' }],
-  ],
-  coverageDirectory: './test-reports/coverage',
-  coveragePathIgnorePatterns: ['/node_modules/', '/tests/', 'locales'],
-  collectCoverageFrom: ['**/*.ts'],
-  coverageReporters: ['text', 'cobertura', 'lcov'],
 };
 
 module.exports = config
